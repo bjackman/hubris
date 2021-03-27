3d1bcb8093b88adefbda8714896b142b53a598be
Brendan Jackman <bhenryj0117@gmail.com>
2021-03-27 17:55:24 +0100

    Delete some stuff from the XML desc that doesn't look very important

--- main.go
+++ main.go
@@ -14,64 +14,15 @@ import (
 )
 
 var xml = `
-<domain type='kvm' id='5'>
+<domain type='kvm'>
   <name>instance-1</name>
-  <uuid>8902ecd3-a6e9-4e59-83d5-4c05a325f67a</uuid>
-  <metadata>
-    <libosinfo:libosinfo xmlns:libosinfo="http://libosinfo.org/xmlns/libvirt/domain/1.0">
-      <libosinfo:os id="http://ubuntu.com/ubuntu/16.04"/>
-    </libosinfo:libosinfo>
-  </metadata>
-  <memory unit='KiB'>1048576</memory>
-  <currentMemory unit='KiB'>1048576</currentMemory>
-  <vcpu placement='static'>1</vcpu>
-  <resource>
-    <partition>/machine</partition>
-  </resource>
+  <memory unit='GiB'>1</memory>
   <os>
-    <type arch='x86_64' machine='pc-i440fx-focal'>hvm</type>
-    <boot dev='hd'/>
+    <type>hvm</type>
   </os>
-  <features>
-    <acpi/>
-    <apic/>
-  </features>
-  <cpu mode='custom' match='exact' check='full'>
-    <model fallback='forbid'>Skylake-Client-IBRS</model>
-    <vendor>Intel</vendor>
-    <feature policy='require' name='ss'/>
-    <feature policy='require' name='vmx'/>
-    <feature policy='require' name='hypervisor'/>
-    <feature policy='require' name='tsc_adjust'/>
-    <feature policy='require' name='clflushopt'/>
-    <feature policy='require' name='umip'/>
-    <feature policy='require' name='md-clear'/>
-    <feature policy='require' name='stibp'/>
-    <feature policy='require' name='arch-capabilities'/>
-    <feature policy='require' name='ssbd'/>
-    <feature policy='require' name='xsaves'/>
-    <feature policy='require' name='pdpe1gb'/>
-    <feature policy='require' name='ibpb'/>
-    <feature policy='require' name='amd-stibp'/>
-    <feature policy='require' name='amd-ssbd'/>
-    <feature policy='require' name='skip-l1dfl-vmentry'/>
-    <feature policy='require' name='pschange-mc-no'/>
-    <feature policy='disable' name='hle'/>
-    <feature policy='disable' name='rtm'/>
-    <feature policy='disable' name='mpx'/>
-  </cpu>
-  <clock offset='utc'>
-    <timer name='rtc' tickpolicy='catchup'/>
-    <timer name='pit' tickpolicy='delay'/>
-    <timer name='hpet' present='no'/>
-  </clock>
   <on_poweroff>destroy</on_poweroff>
   <on_reboot>restart</on_reboot>
   <on_crash>destroy</on_crash>
-  <pm>
-    <suspend-to-mem enabled='no'/>
-    <suspend-to-disk enabled='no'/>
-  </pm>
   <devices>
     <emulator>/usr/bin/qemu-system-x86_64</emulator>
     <disk type='file' device='disk'>
@@ -82,40 +33,8 @@ var xml = `
       <alias name='virtio-disk0'/>
       <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
     </disk>
-    <disk type='file' device='cdrom'>
-      <driver name='qemu' type='raw'/>
-      <source file='/home/brendan/src/hubris-stuff/ubuntu.iso' index='1'/>
-      <backingStore/>
-      <target dev='hda' bus='ide'/>
-      <readonly/>
-      <alias name='ide0-0-0'/>
-      <address type='drive' controller='0' bus='0' target='0' unit='0'/>
-    </disk>
-    <controller type='usb' index='0' model='ich9-ehci1'>
-      <alias name='usb'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x7'/>
-    </controller>
-    <controller type='usb' index='0' model='ich9-uhci1'>
-      <alias name='usb'/>
-      <master startport='0'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0' multifunction='on'/>
-    </controller>
-    <controller type='usb' index='0' model='ich9-uhci2'>
-      <alias name='usb'/>
-      <master startport='2'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x1'/>
-    </controller>
-    <controller type='usb' index='0' model='ich9-uhci3'>
-      <alias name='usb'/>
-      <master startport='4'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x2'/>
-    </controller>
     <controller type='pci' index='0' model='pci-root'>
       <alias name='pci.0'/>
-    </controller>
-    <controller type='ide' index='0'>
-      <alias name='ide'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x01' function='0x1'/>
     </controller>
     <interface type='network'>
       <mac address='52:54:00:be:c2:45'/>
@@ -137,25 +56,7 @@ var xml = `
       <target type='serial' port='0'/>
       <alias name='serial0'/>
     </console>
-    <input type='mouse' bus='ps2'>
-      <alias name='input0'/>
-    </input>
-    <input type='keyboard' bus='ps2'>
-      <alias name='input1'/>
-    </input>
-    <memballoon model='virtio'>
-      <alias name='balloon0'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x0'/>
-    </memballoon>
   </devices>
-  <seclabel type='dynamic' model='apparmor' relabel='yes'>
-    <label>libvirt-8902ecd3-a6e9-4e59-83d5-4c05a325f67a</label>
-    <imagelabel>libvirt-8902ecd3-a6e9-4e59-83d5-4c05a325f67a</imagelabel>
-  </seclabel>
-  <seclabel type='dynamic' model='dac' relabel='yes'>
-    <label>+64055:+108</label>
-    <imagelabel>+64055:+108</imagelabel>
-  </seclabel>
 </domain>
 `
 
